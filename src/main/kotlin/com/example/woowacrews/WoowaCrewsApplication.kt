a20227ba1295d6a917e6ee3d2d1b01689167a5e7
lowoon <sangbo0229@gmail.com>
2020-08-17 17:06:17 +0900

    init

---
+++ src/main/kotlin/com/example/woowacrews/WoowaCrewsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.woowacrews
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WoowaCrewsApplication
+
+fun main(args: Array<String>) {
+    runApplication<WoowaCrewsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/woowacrews/WoowaCrewsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.woowacrews
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WoowaCrewsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
